42bcdb599897373f9550a14d4729bc658a236f51
Yuvraj518 <124797389+Yuvraj518@users.noreply.github.com>
2023-08-22 21:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ let op2=document.getElementById("email");
 let op3=document.getElementById("age");
 let op4=document.getElementById("gpa");
 let op5=document.getElementById("degree");
-let btn=document.querySelector("button");
-console.log(btn)
-let count=0;
+let btn=document.getElementById("btn")
+let btn1=document.getElementById("btn1")
+
 function displayStudents(students){
-    console.log("hii")
+    // console.log("hii",students)
     document.getElementById("tb1").innerHTML=``;
     students.forEach(x => {
       const ele1=document.createElement("tr");
@@ -47,30 +47,32 @@ function displayStudents(students){
         <div class="degree">
           <div class="dg1">${x.degree}</div>
           <div class="dg2">
-            <div class="edit"><img src="./edit 1.png" alt="edit"></div>
-            <div class="del"><img src="./trash-2 1.png" alt="del"></div>
+            <div class="edit" onclick="editFun(${x.ID})"><img src="./edit 1.png" alt="edit"></div>
+            <div class="del"  onclick="delFun(${x.ID})"><img src="./trash-2 1.png" alt="del"></div>
           </div>
         </div>
       </td>`
-      count++;
       document.getElementById("tb1").appendChild(ele1);
     });
 }
 
+let count=4;
 function callAdd(){
-    console.log("clicked")
+    console.log("Adding student function called")
     students.push({
-        ID: `${(students.length)+1}`,
+        ID: `${(count)}`,
         name: `${op1.value}`,
         age: `${op3.value}`,
         grade: `${op4.value}`,
         degree: `${op5.value}`,
         email: `${op2.value}`
       })
+      count++;
       console.log(students)
       displayStudents(students);
 }
 btn.addEventListener("click",callAdd);
+
 
 document.getElementById("search").addEventListener("input",(e)=>{
   const inp1=e.target.value.trim().toLowerCase();
@@ -79,16 +81,44 @@ document.getElementById("search").addEventListener("input",(e)=>{
   displayStudents(filtredStudents);
 });
 
-// document.getElementsByClassName("edit").addEventListener("click",()=>{
-//   console.log("swim")
-// })
-// students.forEach(x => {
-//   x.addEventListener("click",()=>{
-//     students.remove(x);
-//   })
-// });
 
-console.log("okk")
+function editFun(id1){
+  btn.style.display=`none`
+  btn1.style.display=`block`
+  console.log(id1)
+  const st1=students.find((s)=>s.ID===id1);
+  op1.value=st1.name
+  op2.value=st1.email
+  op3.value=st1.age
+  op4.value=st1.grade
+  op5.value=st1.degree
+  btn1.addEventListener("click",()=>{
+    console.log("qa",st1)
+    st1.name=op1.value;
+    st1.email=op2.value
+    st1.age=op3.value
+    st1.grade=op4.value;
+    st1.degree=op5.value
+    btn1.style.display=`none`
+    btn.style.display=`block`
+    displayStudents(students);
+  })
+}
+
+
+
+function delFun(id2){
+  const confirmation=prompt("If you are sure write 'y' or else write 'n'.")
+  if(confirmation.toLowerCase()==`y`){
+    const index1=students.findIndex((s)=>s.ID===id2)
+    students.splice(index1,1);
+    displayStudents(students);
+    alert("Deleted Successfully")
+  }
+}
+
+
+console.log("Let's Start")
 displayStudents(students);
 
 
